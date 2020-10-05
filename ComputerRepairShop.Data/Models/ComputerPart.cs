c3c0b0f6bff65240d33abd5163b28c0a604e0259
Mathijs van de Kerkhof <mathijs.vandekerkhof@itvitaelearning.nl>
2020-10-05 14:12:06 +0200

    Add project files.

---
+++ ComputerRepairShop.Data/Models/ComputerPart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerRepairShop.Data.Models
+{
+    public class ComputerPart
+    {
+        public int Id { get; set; }
+        [Required]
+        public string PartName { get; set; }
+        [Required]
+        public string VendorName { get; set; }
+        public decimal Price { get; set; }
+        public string Status { get; set; }
+    }
+
+}
---
+++ ComputerRepairShop.Data/Models/RepairJob.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerRepairShop.Data.Models
+{
+    public class RepairJob
+    {
+        public int Id { get; set; }
+        public string RepairName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Status { get; set; }
+    }
+}
